6e5e184156b108b58e51e21536a6226f339c336c
Tucker Dickson <nathan-dickson@uiowa.edu>
2023-07-07 10:00:50 -0500

    Initial Commit

---
+++ GymPad/GymPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymPadApp.swift
+//  GymPad
+//
+//  Created by Tucker Dickson on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GymPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
